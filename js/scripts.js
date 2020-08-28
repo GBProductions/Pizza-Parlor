7b3f06bcd255851ae6d23e7641abfeae2d28c09b
Garrett Brown <garrettpaulbrown@gmail.com>
2020-08-28 13:29:01 -0700

    Add JS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,3 @@
-/*Pricing logic
-
-High Price - $20 =>7
-Medium Price- $15 =>4
-Low price- $10 =>1
-
-Small-1
-Medium-2
-Large-4
-
-None - 0
-Pepperoni-1
-Ham-2
-Bacon-3
-
-None - 0
-Pineapple-1
-Mushrooms-2
-Olives-3
-
-*/
-
-
 //Business Logic for Pizza
 function Pizza() {
   this.orders = [],
