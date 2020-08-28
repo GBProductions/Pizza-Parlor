71c596516d53d76bed61f0c6463e407450b09725
Garrett Brown <garrettpaulbrown@gmail.com>
2020-08-28 12:01:36 -0700

    Add JS, new Pizza.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,15 @@
-//Business Logic
-
+//Business Logic for Pizza
 
 function Pizza(size, meat, veggie) {
   this.size = size,
   this.meat = meat,
   this.veggie = veggie,
   this.pizzaOrder = function() {
-    return "A " + this.size + " pizza with" + this.meat + " and " + this.veggie;
+    return "A " + this.size + " pizza with " + this.meat + " and " + this.veggie;
   } 
 }
 
-var pizza1 = new Pizza("medium", "pepperoni", "pineapple");
-
-console.log(pizza1);
-
-
+let 
 
 //UI Logic
 
@@ -22,9 +17,13 @@ console.log(pizza1);
 $(document).ready(function() {
   $("form#pizzaSelect").submit(function(event) {
     event.preventDefault();
-    const size = ($("input:radio[name=pizzaSize]:checked").val());
-    const meat = ($("input:radio[name=pizzaTopping1]:checked").val());
-    const veggie = ($("input:radio[name=pizzaTopping2]:checked").val());
+    const inputtedSize = ($("input:radio[name=pizzaSize]:checked").val());
+    const inputtedMeat = ($("input:radio[name=pizzaTopping1]:checked").val());
+    const inputtedVeggie = ($("input:radio[name=pizzaTopping2]:checked").val());
     console.log(size, meat, veggie);
+
+
+
+    let pizzaOrder = new Pizza(inputtedSize, inputtedMeat, inputtedVeggie);
   });
 });
