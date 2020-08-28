22eaf31bb1f6e26bb4affc06a71d1117d262012b
Garrett Brown <garrettpaulbrown@gmail.com>
2020-08-28 15:24:15 -0700

    Add JS. Change input const values for size, meat, and veggie.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,63 @@
-//Business Pizza Order Logic
+//Business Logic Overal Pizza Order
   function OverallPizzaOrder() {
-    this.pizza = []
+    this.pizzas = [];
+    this.currentId = 0;
   }
+
+  OverallPizzaOrder.prototype.addPizza = function(pizza) {
+    pizza.id = this.assignId();
+    this.pizzas.push(pizza);
+  }
+
+  OverallPizzaOrder.prototype.assignId = function() {
+    this.currentId +=1;
+    return this.currentId;
+  }
+
+  OverallPizzaOrder.prototype.findPizza = function(id) {
+    for (let i=0; i< this.pizzas.length; i++) {
+        if (this.pizzas[i].id == id) {
+          return this.pizzas[i];
+        }
+      };
+    return false
+  }
+
+//Business Logic for Pizza
+function Pizza(size, meat, veggie) {
+  this.size = size;
+  this.meat = meat;
+  this.veggie = veggie;
+}
+
+Pizza.prototype.fullOrder = function() {
+  return ("A " + this.size + " pizza with " + this.meat + " and " + this.veggie);
+}
+
+
+//User Interface Logic------------
+let overallPizzaOrder =  new OverallPizzaOrder();
+
+$(document).ready(function() {
+  $("form#pizzaSelect").submit(function(event) {
+    event.preventDefault();
+    const inputtedSize = ($("input:radio[name=pizzaSize]:checked").val());
+    const inputtedMeat = ($("input:radio[name=pizzaTopping1]:checked").val());
+    const inputtedVeggie = ($("input:radio[name=pizzaTopping2]:checked").val());
+    let newPizza = new Pizza(inputtedSize, inputtedMeat, inputtedVeggie);
+    overallPizzaOrder.addPizza(newPizza);
+    console.log(overallPizzaOrder.pizzas)
+  });
+})
+
+
+
+
+/* 
+let OverallPizzaOrder = new OverallPizzaOrder();
+let pizza = new Pizza("Small", "Ham", "Olives");
+OverallPizzaOrder.addPizza(pizza);
+
 
   OverallPizzaOrder.prototype.addPizza = function() {
     pizza.id = this.assignId();
@@ -22,17 +78,6 @@
       return $20
   }
 
-//Business Logic for Pizza
-this.pizza = Pizza 
-function Pizza(size, meat, veggie) {
-  this.size = size,
-  this.meat = meat,
-  this.veggie = veggie
-}
-
-Pizza.prototype.fullOrder = function() {
-  return "A " + this.size + " pizza with " + this.meat + " and " + this.veggie;
-}
 
 
 
@@ -45,18 +90,4 @@ function displayPizza(pizzaId) {
   $(".final-price").html(newPizza)
 }
 
-
-$(document).ready(function() {
-  $("form#pizzaSelect").submit(function(event) {
-    event.preventDefault();
-    const size = ($("input:radio[name=pizzaSize]:checked").val());
-    const meat = ($("input:radio[name=pizzaTopping1]:checked").val());
-    const veggie = ($("input:radio[name=pizzaTopping2]:checked").val());
-    console.log(size, meat, veggie);
-
-
-    let newPizza = new Pizza(size, meat, veggie);
-
-    //OverallPizzaOrder.addPizza(newPizza)
-  });
-})
+*/
