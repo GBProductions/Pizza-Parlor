5d53b324e7432a58ea0b871868ac122083d3ce06
Garrett Brown <garrettpaulbrown@gmail.com>
2020-08-28 12:13:01 -0700

    Add pricing logic in JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,44 @@
+/*Pricing logic
+
+High Price - $20
+Medium Price- $15
+Low price- $10
+
+Small-1
+Medium-2
+Large-3
+
+Pepperoni-1
+Ham-2
+Bacon-3
+
+Pineapple-1
+Mushrooms-2
+Olives-3
+
+
+
+*/
+
+
 //Business Logic for Pizza
+function Pizza() {
+  this.orders = [],
+  this.currentId = 0
+}
+
+Pizza.prototype
 
 function Pizza(size, meat, veggie) {
   this.size = size,
   this.meat = meat,
   this.veggie = veggie,
+
   this.pizzaOrder = function() {
     return "A " + this.size + " pizza with " + this.meat + " and " + this.veggie;
   } 
 }
 
-let 
 
 //UI Logic
 
