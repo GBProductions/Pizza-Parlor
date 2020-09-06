3b53c71ee0e5712e35777640f65e86b78e757fd0
Garrett Brown <garrettpaulbrown@gmail.com>
2020-09-06 15:59:16 -0700

    Rewrite Pizza function.

--- js/scripts.js
+++ js/scripts.js
@@ -22,23 +22,32 @@ Olives-3
 
 
 //Business Logic Overal Pizza Order
+function Pizza(newPizza) {
+  this.newPizza = newPizza
+}
+
+  OverallPizzaOrder.prototype.findPrice = function(id) {
+    if (this.size === "Small")
+      return $10
+    if (this.size === "Medium")
+      return $15
+    if (this.size === "Large")
+      return $20
+    });
+/*
   function OverallPizzaOrder() {
     this.pizzas = [];
   }
 
-function Pizza(size, meat, veggie) {
-  this.size = size;
-  this.meat = meat;
-  this.veggie = veggie;
-}
+
 
 Pizza.prototype.fullOrder = function() {
   return ("A " + this.size + " pizza with " + this.meat + " and " + this.veggie);
 }
 
-
+*/
 //User Interface Logic------------
-let overallPizzaOrder =  new OverallPizzaOrder();
+//let overallPizzaOrder =  new OverallPizzaOrder();
 
 $(document).ready(function() {
   $("form#pizzaSelect").submit(function(event) {
@@ -48,10 +57,13 @@ $(document).ready(function() {
     let inputtedSize = parseInt($("#pizzaSize").val());
     let inputtedMeat = parseInt($("#pizzaTopping1").val());
     let inputtedVeggie = parseInt($("#pizzaTopping2").val());
-    console.log(inputtedSize + inputtedMeat + inputtedVeggie)
     
-    let newPizza = new Pizza(inputtedSize, inputtedMeat, inputtedVeggie);
-    overallPizzaOrder.addPizza(newPizza);
+    
+    let newPizza = new Pizza(inputtedSize + inputtedMeat + inputtedVeggie);
+    console.log(newPizza);
+    
+    
+    //overallPizzaOrder.addPizza(newPizza);
 
 })
 
@@ -59,19 +71,11 @@ $(document).ready(function() {
 
 
 
-let pizza = new Pizza("Small", "Ham", "Olives"); 
+/*let pizza = new Pizza("Small", "Ham", "Olives"); 
 OverallPizzaOrder.addPizza(pizza);
 
 
 
-  OverallPizzaOrder.prototype.findPrice = function(id) {
-    if (this.size === "Small")
-      return $10
-    if (this.size === "Medium")
-      return $15
-    if (this.size === "Large")
-      return $20
-  }
-});
+
 
 
