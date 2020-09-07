48e54ce8fbfc7bfd7ebc7f19cc1bc74cdbe5d790
Garrett Brown <garrettpaulbrown@gmail.com>
2020-09-06 23:25:44 -0700

    Change JS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,17 @@
-/*Pricing logic
-
-High Price - $20 =>7
-Medium Price- $15 =>4
-Low price- $10 =>1
-
-Small-1
-Medium-2
-Large-4
-
-None - 0
-Pepperoni-1
-Ham-2
-Bacon-3
-
-None - 0
-Pineapple-1
-Mushrooms-2
-Olives-3
-
-
-Pizza.prototype.fullOrder = function() {
-  return ("A " + this.size + " pizza with " + this.meat + " and " + this.veggie);
-}
-*/
-
-
-
-
-//Business Logic Overal Pizza Order
-function Pizza(newPizza) {
-  this.newPizza = newPizza
+//Business Logic-----------
+function addPizza(inputtedSize, inputtedMeat, inputtedVeggie) {
+  return (inputtedSize + inputtedMeat + inputtedVeggie)
 }
 
-function find() {
-  if (newPizza >= 1) {
-    $('#ten-dollars').show(); 
-    } else {
-    $('#ten-dollars').hide();
-    } 
-  if (Pizza >= 4) {
-    $('#fifteen-dollars').show()
-    } else {
-      $('#fifteen-dollars').hide();
-    }   
-  if(Pizza >= 7) { 
-    $('#twenty-dollars').show();
-    } else {
-    $('#twenty-dollars').hide();
-    }  
-};
-
+function addValues(newPizza) {
+    if (newPizza >= 7) {
+      $('#twenty-dollars').show()
+    } else if (newPizza >= 4) {
+      $('#fifteen-dollars').show()
+    } else if (newPizza >= 1) {
+      $('#ten-dollars').show()
+    }
+  };
 
 //User Interface Logic------------
 
@@ -61,13 +23,14 @@ $(document).ready(function() {
     let inputtedSize = parseInt($("#pizzaSize").val());
     let inputtedMeat = parseInt($("#pizzaTopping1").val());
     let inputtedVeggie = parseInt($("#pizzaTopping2").val());
-    
-    debugger
-    let newPizza = new Pizza(inputtedSize + inputtedMeat + inputtedVeggie);
-    
-    (find(Pizza(newPizza)));
-  
 
+    let newPizza = addPizza(inputtedSize, inputtedMeat, inputtedVeggie); 
+    
+    (addValues(newPizza))
 
-  });
+    
+    
+    
+
+  })
 });
